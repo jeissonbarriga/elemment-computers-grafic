d6306edd5b81039c4ff19f9d714e21a24d26f0e0
EdisonHuerfano <ecamilo10hue@gmail.com>
2020-02-07 13:47:10 -0500

    Se agrega función find() a clase BinarySearchTree, 2

--- src/app/data-structures/binary-search-tree.ts
+++ src/app/data-structures/binary-search-tree.ts
@@ -10,7 +10,23 @@ export class BinarySearchTree<T> {
     
     constructor(){
         this.root = null;
-    }   
+    }    
+
+    find(key: T, root: TreeNode<T>){
+        if(root.key = key){
+            return root;
+        }
+        else if(root.key > key){
+            if(root.left != null)
+                return this.find(key, root.left);
+            return root;
+        }
+        else if(root.key < key){
+            if(root.right != null)
+                return this.find(key, root.right);
+            return root;
+        }
+    }
       
 
-}
+} 
